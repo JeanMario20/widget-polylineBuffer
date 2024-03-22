3a7e6fa2b18240d39e67b152e03e3ed1b9e65f32
hbaeza <hbaeza@sigsa.info>
2024-03-22 09:03:13 -0600

    integracion de funcion agregar cdatos

--- setting/nls/strings.js
+++ setting/nls/strings.js
@@ -1,5 +1,25 @@
 define({
 	root: ({
-		configText: "Set config text:"
+		// configText: "Set config text:",
+		numPerPage: "Number of items per page",
+    scopeOptions: {
+      "defaultScope": "Default search scope",
+      "labelPlaceholder": "Optional label",
+      "MyContent": "Allow My Content",
+      "MyOrganization": "Allow My Organization",
+      "ArcGISOnline": "Allow ArcGIS Online",
+      "Curated": "Allow Curated",
+      "CuratedFilter": "Curated filter",
+      "livingAtlasExample": "Living atlas example:"
+    },
+    addFromUrl: {
+      caption: "Allow URL"
+    },
+    addFromFile: {
+      caption: "Allow File",
+      maxRecordCount: "Maximum records per file"
+    },
+    _default: "Default",
+    makeDefault: "Make default"
   })
 });
